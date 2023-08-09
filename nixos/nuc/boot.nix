edb68e1fc27b790322b05425bd7dadd4a16ae8bb
Bruce Stringer <buggerxenocide@gmail.com>
2023-08-09 17:54:59 -0500

    Nuc cleanup

---
+++ nixos/nuc/boot.nix
@@ -0,0 +1,9 @@
+{
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  boot.initrd.availableKernelModules = [ "nvme" "xhci_pci" "thunderbolt" "usbhid" "usb_storage" "sd_mod" ];
+  boot.initrd.kernelModules = [ "amdgpu" ];
+  boot.kernelModules = [ "kvm-amd" ];
+  boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+}
--- nixos/nuc/configuration.nix
+++ nixos/nuc/configuration.nix
@@ -1,15 +1,20 @@
 # This is your system's configuration file.
 # Use this to configure your system environment (it replaces /etc/nixos/configuration.nix)
 
-{ inputs, outputs, lib, config, pkgs, ... }: {
+{ inputs, outputs, lib, config, pkgs, ... }:
+let
+  authKeys = import ./authKeys.nix;
+in
+{
   # You can import other NixOS modules here
   imports = [
     # If you want to use modules your own flake exports (from modules/nixos):
     # outputs.nixosModules.example
 
     # Or modules from other flakes (such as nixos-hardware):
-    inputs.hardware.nixosModules.common-cpu-amd
-    inputs.hardware.nixosModules.common-ssd
+    inputs.nixos-hardware.nixosModules.common-cpu-amd
+    inputs.nixos-hardware.nixosModules.common-gpu-amd
+    inputs.nixos-hardware.nixosModules.common-pc-ssd
 
     # You can also split up your configuration and import pieces of it here:
     # ./users.nix
@@ -18,7 +23,8 @@
     ./hardware-configuration.nix
     ./filesystems.nix
     ./networking.nix
-    ./authKeys.nix
+    ./boot.nix
+
   ];
 
   nixpkgs = {
@@ -63,28 +69,16 @@
     };
   };
 
-  # FIXME: Add the rest of your current configuration
-
-  # TODO: Set your hostname
   networking.hostName = "nuc";
 
-  # TODO: This is just an example, be sure to use whatever bootloader you prefer
-#  boot.loader.systemd-boot.enable = true;
 
-  # TODO: Configure your system-wide user settings (groups, etc), add more users as needed.
   users.users = {
-    # FIXME: Replace with your username
     root = {
-      openssh.authorizedKeys.keys = keys;
+      openssh.authorizedKeys.keys = authKeys.authKeys;
     };
     gifflen = {
       isNormalUser = true;
-      openssh.authorizedKeys.keys = keys;
-#      openssh.authorizedKeys.keys = [
-#
-#        "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCgWlHfJrGw6xYcVRSklSOLLykTDrkIEc0VU8lCc7uQK6DbLFFM5s3RSg11g6sDoQ6G3unFVQmBHL/Iq/QhzGfR7/2SgQjPp7fjJCHQXFpRgShqC6MP5gOL5LyfL+xGDKzaw2ZQatJhp8jA/KHpJeHW6lVdDZAeNC/mDr54VBimjc7CNgCkvGJ+/dJfKhuMZ76FU0nrR+8f9nrPQeRyE0AJP42W+XQBYmXebcJrzh3wiLRXzNVSl7uj2N4Jt3ovI0qGAuJ3ko7eyA5eJawnU5m7lNIXnw8Vj2qElz1yHo7TLlmOX4gb05yG4o466f5Hm2zo+Pv6PNR0tk41coC0RFKE6vjrPaQfzDAOMQ+vgdSUTk+D2QHqjlh4sRRPOe3IijkmLG6bRSTnI9sBu6DhQsNYFErvYBvicurGZ9rr5fbXh2Yr3bFK6qFwwwSwTv5eIW3JmwmHJilaNDDJwZ2rgnnsOycyFFs+tY/wd8V8EssIiu5mO3wJPNZH55TvVdp5SSE= gifflen@werkwerk"
-#      ];
-      # TODO: Be sure to add any other groups you need (such as networkmanager, audio, docker, etc)
+      openssh.authorizedKeys.keys = authKeys.authKeys;
       extraGroups = [ "wheel" "networkmanager" "docker" ];
     };
   };
@@ -94,33 +88,18 @@
   services.openssh = {
     enable = true;
     # Forbid root login through SSH.
-    # permitRootLogin = "no";
+    permitRootLogin = "no";
     # Use keys only. Remove if you want to SSH using password (not recommended)
-    # passwordAuthentication = false;
+    passwordAuthentication = false;
   };
 
   # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
   system.stateVersion = "23.05";
 
-  boot.initrd.availableKernelModules = [
-    "nvme"
-    "xhci_pci"
-    "ahci"
-    "usbhid"
-    "usb_storage"
-    "sd_mod"
-  ];
-  boot.initrd.kernelModules = [
-    "amdgpu"
-  ];
-  boot.kernelModules = [
-    "kvm-amd"
-  ];
-  boot.extraModulePackages = [ ];
 
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 
-    nix.settings.trusted-users = [
+  nix.settings.trusted-users = [
     "root"
     "gifflen"
     "@wheel"
